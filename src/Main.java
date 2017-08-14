478de9f583727dcbc36c787ed1d87c2c7a085ce1
Michael <michael@thegur.net>
2017-08-14 22:49:41 +0300

    temporarily reverted to a simpler grammar

--- src/Main.java
+++ src/Main.java
@@ -21,34 +21,17 @@ public class Main
     //      After That Shite Is Done, Go Over The Code In Parser & Grammar, And Clean It.
     //          Add Some Documentation In Complicated Parts.
     public static void main(String[] args) {
-        String code =
-                "class poo {" +
-                "   void poopoo {" +
-                "       int a = (\"poo\" + \"poo\")[23.12 + 5 * 4] + 2 ; # some clever remark\n" +
-                "   }" +
-                "}";
+        String code = "1 + 2 * 4";
         TokenStream t = new TokenStream(code);
 
-        Symbol S = new Symbol("STMT"), B = new Symbol("BLOCK"),
-                E = new Symbol("EXPR"), T = new Symbol("T"), F = new Symbol("F"), O = new Symbol("O");
+        Symbol E = new Symbol("EXPR"), T = new Symbol("T"), F = new Symbol("F");
 
-        B.addProduction().addProduction(Terminal.END_OF_INPUT).addProduction(S).addProduction(B, S);
-        O.addProduction(Token.Type.NUMBER.t).addProduction(Token.Type.IDENTIFIER.t)
-        .addProduction(Token.Type.STRING.t).addProduction(Token.Type.TRUE.t).addProduction(Token.Type.FALSE.t);
-        // type name = EXPR ;
-        S.addProduction(Token.Type.IDENTIFIER.t, Token.Type.IDENTIFIER.t, Token.Type.EQUALS.t, E, Token.Type.SEMICOLON.t);
         E.addProduction(T).addProduction(E, Token.Type.OPEN_BRACKET.t, E, Token.Type.CLOSE_BRACKET.t)
         .addProduction(E, Token.Type.PLUS.t, T).addProduction(E, Token.Type.DASH.t, T);
         T.addProduction(T, Token.Type.ASTERISK.t, F).addProduction(T, Token.Type.SLASH.t, F).addProduction(F);
-        F.addProduction(Token.Type.OPEN_PAREN.t, E, Token.Type.CLOSE_PAREN.t).addProduction(O);
-        // function definition
-        // return_type name { BLOCK }
-        S.addProduction(Token.Type.IDENTIFIER.t, Token.Type.IDENTIFIER.t, Token.Type.OPEN_CURLY.t, B, Token.Type.CLOSE_CURLY.t)
-        // class definition
-        // class name { BLOCK }
-        .addProduction(Token.Type.CLASS.t, Token.Type.IDENTIFIER.t, Token.Type.OPEN_CURLY.t, B, Token.Type.CLOSE_CURLY.t);
+        F.addProduction(Token.Type.OPEN_PAREN.t, E, Token.Type.CLOSE_PAREN.t).addProduction(Token.Type.NUMBER.t);
 
-        Grammar g = new Grammar(B);
+        Grammar g = new Grammar(E);
         System.out.println(g);
         System.out.println();
 
